0b641d60ece9d9ccb3eee623e1ae329a381eb3d4
Boper <85123713+boprr@users.noreply.github.com>
2025-01-01 15:05:09 +0100

    forgot to flush out the old gdt and to commit pic driver

---
+++ src/include/dri/pic.h
@@ -0,0 +1,19 @@
+#ifndef PIC_H
+#define PIC_H
+
+#include <stdint.h>
+
+#include "utils.h"
+
+#define PIC1 0x20 /* IO base address for master PIC */
+#define PIC2 0xA0 /* IO base address for slave PIC */
+#define PIC1_COMMAND PIC1
+#define PIC1_DATA (PIC1 + 1)
+#define PIC2_COMMAND PIC2
+#define PIC2_DATA (PIC2 + 1)
+#define PIC_EOI 0x20
+
+void pic_eoi(uint8_t irq);
+void pic_remap();
+
+#endif
