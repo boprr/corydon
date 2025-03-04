0ed30ff32461c3b4a145ce4423bc308390a572e9
boprr <85123713+boprr@users.noreply.github.com>
2025-03-04 19:49:38 +0100

    pmm fix

--- src/arch/x86_64/mem/pmm.c
+++ src/arch/x86_64/mem/pmm.c
@@ -1,4 +1,5 @@
 #include "mem/pmm.h"
+
 mem_bitmap *phys;
 struct multiboot_tag_mmap *mmap_tag = {0};
 
@@ -6,11 +7,10 @@ void parse_mb2_mmap() {
     struct multiboot_tag *tag = (struct multiboot_tag *)(multiboot_data + 8);
 
     while (tag->type != MULTIBOOT_TAG_TYPE_END) {
-        tag = (struct multiboot_tag *)((multiboot_uint8_t *)tag +
-                                       ((tag->size + 7) & ~7));
         switch (tag->type) {
             case MULTIBOOT_TAG_TYPE_MMAP: {
                 mmap_tag = ((struct multiboot_tag_mmap *)tag);
+                break;
             }
             case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO: {
                 struct multiboot_tag_basic_meminfo *basicmem;
@@ -19,18 +19,20 @@ void parse_mb2_mmap() {
                 break;
             }
         }
+
+        tag = (struct multiboot_tag *)((multiboot_uint8_t *)tag +
+                                       ((tag->size + 7) & ~7));
     }
 
     int found_map_start = 0;
     int found_mem_start = 0;
     uint64_t mem_avil = 0;
-    multiboot_memory_map_t *mmap;
 
-    for (mmap = ((struct multiboot_tag_mmap *)mmap_tag)->entries;
-         (multiboot_uint8_t *)mmap <
-         (multiboot_uint8_t *)mmap_tag + mmap_tag->size;
-         mmap = (multiboot_memory_map_t *)((unsigned long)mmap +
-                                           mmap_tag->entry_size)) {
+    multiboot_memory_map_t *mmap =
+        ((struct multiboot_tag_mmap *)mmap_tag)->entries;
+    uint8_t *mmap_end = (uint8_t *)mmap_tag + mmap_tag->size;
+
+    while ((uint8_t *)mmap < mmap_end) {
         if (mmap->type == MULTIBOOT_MEMORY_AVAILABLE && !found_mem_start) {
             found_mem_start = 1;
             phys->mem_start = mmap->addr;
@@ -41,6 +43,9 @@ void parse_mb2_mmap() {
             phys->map = (uint8_t *)mmap->addr;
         }
         if (mmap->type == MULTIBOOT_MEMORY_AVAILABLE) mem_avil += mmap->len;
+
+        mmap =
+            (multiboot_memory_map_t *)((uint64_t)mmap + mmap_tag->entry_size);
     }
 
     if (!found_map_start) panic("Not enough memory");
@@ -52,17 +57,20 @@ void pmm_init() {
     parse_mb2_mmap();
 
     memset(phys->map, 0xff, (phys->size / BLOCK_SIZE));
-    multiboot_memory_map_t *mmap;
-    for (mmap = ((struct multiboot_tag_mmap *)mmap_tag)->entries;
-         (multiboot_uint8_t *)mmap <
-         (multiboot_uint8_t *)mmap_tag + mmap_tag->size;
-         mmap = (multiboot_memory_map_t *)((unsigned long)mmap +
-                                           mmap_tag->entry_size)) {
+
+    multiboot_memory_map_t *mmap =
+        ((struct multiboot_tag_mmap *)mmap_tag)->entries;
+    uint8_t *mmap_end = (uint8_t *)mmap_tag + mmap_tag->size;
+
+    while ((uint8_t *)mmap < mmap_end) {
         if (mmap->type == MULTIBOOT_MEMORY_AVAILABLE) {
             bitmap_mark_region(
                 phys, (void *)mmap->addr, mmap->len / BLOCK_SIZE, 0
             );
         }
+
+        mmap =
+            (multiboot_memory_map_t *)((uint64_t)mmap + mmap_tag->entry_size);
     }
 
     bitmap_mark_region(
